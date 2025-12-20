270004d2e74bf8bcea084b64f4069c98d090882e
NIKHILCHAUHAN87667 <nikhilchauhan87667@gmail.com>
2025-12-20 22:21:31 +0530

    Update appointment.routes.js

--- backend/routes/appointment.routes.js
+++ backend/routes/appointment.routes.js
@@ -1,11 +1,34 @@
 const { Router } = require("express");
-const { getAllAppointments } = require("../controllers/appointment.controller.js");
-
+const {
+  getAllAppointments,
+  createAppointment,
+  updateAppointment,
+  deleteAppointment
+} = require("../controllers/appointment.controller");
 const router = Router();
 
 /**
  * GET /api/appointments/all-appointments
  */
 router.get("/all-appointments", getAllAppointments);
+
+
+
+
+
+/**
+ * CREATE
+ */
+router.post("/create-appointment", createAppointment);
+
+/**
+ * UPDATE
+ */
+router.put("/update/:id", updateAppointment);
+
+/**
+ * DELETE
+ */
+router.delete("/:id", deleteAppointment);
 
 module.exports = { appointmentRouter: router };
